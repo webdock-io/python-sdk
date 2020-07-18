319f33358d8fe181a67a99f231da026d31a3cdf5
Rehmat Alam <contact@rehmat.works>
2020-07-18 20:59:51 +0500

    More test cases

--- tests/TestWebdock.py
+++ tests/TestWebdock.py
@@ -5,6 +5,9 @@ from webdock.webdock import Webdock
 # This server should be created first
 server_slug = 'pythontest1'
 
+# This is random server slug that should not exist
+bad_serverslug = 'a-random-server-that-should-not-exist'
+
 wd = Webdock(os.environ.get('WEBDOCK_API_TOKEN'))
 
 class TestWebdock(unittest.TestCase):
@@ -21,12 +24,27 @@ class TestWebdock(unittest.TestCase):
     # Test non-existent server
     def test_bad_server(self):
         with self.assertRaises(Exception):
-            wd.get_server('a-random-server-that-should-not-exist')
+            wd.get_server(bad_serverslug)
     
     # Test existing server
     def test_good_server(self):
         res = wd.get_server(server_slug)
         self.assertEqual(200, res.get('status'))
+    
+    # Test shell user creation
+    def test_create_shell_user(self):
+        res = wd.create_shelluser(server_slug, 'theuser', 'MySecurePassword?123#', group='sudo', shell='/bin/bash', publicKeys=[])
+        self.assertEqual(200, res.get('status'))
+    
+    # Test shell user listing
+    def test_shell_users(self):
+        res = wd.get_shellusers(server_slug)
+        self.assertEqual(200, res.get('status'))
+    
+    # Test shell users for non-existent server
+    def test_bad_shell_users(self):
+        with self.assertRaises(Exception):
+            wd.get_shellusers(bad_serverslug)
 
 
 if __name__ == '__main__':
