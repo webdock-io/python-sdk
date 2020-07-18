9c91e7e6c1f8597563165245ecfa6083bec589f2
Rehmat Alam <contact@rehmat.works>
2020-07-18 10:58:54 +0500

    Cleaning

--- webdock/webdock.py
+++ webdock/webdock.py
@@ -2,18 +2,13 @@ import requests, json
 
 class Webdock:
 
-    def __init__(self, clientId, apiKey):
-        self.BaseUrl = 'https://api.serverpilot.io/v1'
-        self.Headers = {
-            'Content-Type': 'application/json'
+    def __init__(self, apiToken):
+        self.BaseUrl = 'https://api.webdock.io'
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {}'.format(apiToken)
         }
-        self.endpoints = {
-            'servers': 'servers',
-            'users': 'sysusers',
-            'apps': 'apps',
-            'databases': 'dbs',
-            'actions': 'actions'
-        }
+        self.endpoints = {}
 
     def send_response(self, res, json=True):
         if res.status_code in [200, 201]:
@@ -26,121 +21,9 @@ class Webdock:
 
     def make_request(self, endpoint, requestType='GET', data=None):
         if requestType == 'GET':
-            res = requests.get('{}/{}'.format(self.BaseUrl, endpoint), auth=self.Auth)
+            res = requests.get('{}/{}'.format(self.BaseUrl, endpoint), headers=self.headers)
         elif requestType == 'POST':
-            res = requests.post('{}/{}'.format(self.BaseUrl, endpoint), data=json.dumps(data), auth=self.Auth, headers=self.Headers)
+            res = requests.post('{}/{}'.format(self.BaseUrl, endpoint), data=json.dumps(data), headers=self.headers)
         elif requestType == 'DELETE':
-            res = requests.delete('{}/{}'.format(self.BaseUrl, endpoint), auth=self.Auth)
+            res = requests.delete('{}/{}'.format(self.BaseUrl, endpoint), headers=self.headers)
         return self.send_response(res)
-
-    # Servers
-    def list_servers(self):
-        return self.make_request('{}'.format(self.endpoints.get('servers')))
-
-    def create_server(self, data):
-        # data = {'name': 'myserver', 'plan': 'business', 'enable_ssh_password_auth': True}
-        params = ['name']
-        for item in params:
-            if not item in data:
-                raise Exception('{} is a required parameter.'.format(item))
-        return self.make_request('{}'.format(self.endpoints.get('servers')), requestType='POST', data=data)
-
-    def get_server(self, serverId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('servers'), serverId))
-
-    def update_server(self, serverId, data):
-        # i.e. data = {'firewall': False}
-        return self.make_request('{}/{}'.format(self.endpoints.get('servers'), serverId), requestType='POST', data=data)
-
-    def delete_server(self, serverId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('servers'), serverId), requestType='DELETE')
-
-    # Sys users
-    def list_users(self):
-        return self.make_request('{}'.format(self.endpoints.get('users')))
-
-    def create_user(self, serverId, data):
-        # data = {'name': 'newsysuser', 'password': 'sysuserpass'}
-        params = ['name', 'password']
-        for item in params:
-            if not item in data:
-                raise Exception('{} is a required parameter.'.format(item))
-        data['serverid'] = serverId
-        return self.make_request('{}'.format(self.endpoints.get('users')), requestType='POST', data=data)
-
-    def get_user(self, userId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('users'), userId))
-
-    def delete_user(self, userId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('users'), userId), requestType='DELETE')
-
-    def update_user(self, userId, data):
-        # data = {'password': 'newpassword'}
-        params = ['password']
-        for item in params:
-            if not item in data:
-                raise Exception('{} is a required parameter.'.format(item))
-        return self.make_request('{}/{}'.format(self.endpoints.get('users'), userId), requestType='POST', data=data)
-
-    # Apps
-    def list_apps(self):
-        return self.make_request('{}'.format(self.endpoints.get('apps')))
-
-    def create_app(self, data):
-        params = ['name', 'sysuserid', 'runtime']
-        for item in params:
-            if not item in data:
-                raise Exception('{} is a required parameter.'.format(item))
-        return self.make_request('{}'.format(self.endpoints.get('apps')), requestType='POST', data=data)
-
-    def get_app(self, appId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('apps'), appId))
-
-    def update_app(self, appId, data):
-        # i.e. data = {'runtime': 'php7.3'}
-        return self.make_request('{}/{}'.format(self.endpoints.get('apps'), appId), requestType='POST', data=data)
-
-    def enable_ssl(self, appId, data=None):
-        # data = {'key': 'privateKey', 'cert': 'SSLCertContent', 'cacerts': 'CACertsIfApplicable'}
-        if data is None:
-            # Use auto-SSL
-            data = data={'auto': True}
-        return self.make_request('{}/{}/ssl'.format(self.endpoints.get('apps'), appId), requestType='POST', data=data)
-
-    def force_ssl(self, appId):
-        return self.make_request('{}/{}/ssl'.format(self.endpoints.get('apps'), appId), requestType='POST', data={'force': True})
-
-    def disable_ssl(self, appId):
-        return self.make_request('{}/{}/ssl'.format(self.endpoints.get('apps'), appId), requestType='DELETE')
-
-    def delete_app(self, appId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('apps'), appId), requestType='DELETE')
-
-    # Databases
-    def list_databases(self):
-        return self.make_request('{}'.format(self.endpoints.get('databases')))
-
-    def create_database(self, data):
-        params = ['appid', 'name', 'user']
-        # user must be a dictionary: {'user': {'name': 'dbusername', 'password': 'dbpassword'}}
-        for item in params:
-            if not item in data:
-                raise Exception('{} is a required parameter.'.format(item))
-        return self.make_request('{}'.format(self.endpoints.get('databases')), requestType='POST', data=data)
-
-    def update_database(self, databaseId, data):
-        params = ['user']
-        # user must be a dictionary: {'user': {'id': 'userId', 'password': 'newdbpassword'}}
-        for item in params:
-            if not item in data:
-                raise Exception('{} is a required parameter.'.format(item))
-        return self.make_request('{}/{}'.format(self.endpoints.get('databases'), databaseId), requestType='POST', data=data)
-
-    def get_database(self, databaseId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('databases'), databaseId))
-
-    def delete_database(self, databaseId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('databases'), databaseId), requestType='DELETE')
-
-    def check_action(self, actionId):
-        return self.make_request('{}/{}'.format(self.endpoints.get('actions'), actionId))
